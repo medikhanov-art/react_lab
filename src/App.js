b901b863d86aaa94821f5502c7f34e175854d282
medikhanov-art <medikhanov_a@iuca.kg>
2025-12-01 06:10:47 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import movieData from './movieData';
 
 function App() {
+
+  const [selectedMovie, setSelectedMovie] = useState(null);
+  
+  const handleMovieClick = (movie) => {
+    setSelectedMovie(movie);
+  };
+
+  const handleBackClick = () => {
+    setSelectedMovie(null);
+  }
+
+  if (selectedMovie) {
+    return (
+      <div className="App"> 
+        <button onClick={handleBackClick}> back to table </button>
+        <div className="movie-detail">
+          <img src={selectedMovie.poster} alt={selectedMovie.title} />
+          <div className="detail-info">
+            <h1>{selectedMovie.title}</h1>
+            <p><strong>Режиссер:</strong> {selectedMovie.director}</p>
+            <p><strong>Год:</strong> {selectedMovie.year}</p>
+            <p><stromg>Рейтинг:</stromg> ⭐ {selectedMovie.rating}/10</p>
+            <p><strong>Описание:</strong> {selectedMovie.description}</p>
+          </div>
+        </div>
+      </div>
+    );
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Мой Кинокаталог</h1>
+      <div className="movie-list">
+        {movieData.map(movie => (
+          <div
+            key={movie.id}
+            className="movie-card"
+            onClick={() => handleMovieClick(movie)}
+          >
+            <img src={movie.poster} alt={movie.title} />
+            <h3>{movie.title}</h3>
+            <p>{movie.year}</p>
+            <small>⭐ {movie.rating}</small>  
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
